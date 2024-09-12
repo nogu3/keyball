0f0a4902375353029f1fc5fd79fd2f9355160c4d
shoya.noguchi <nogukids@gmail.com>
2024-09-12 21:49:56 +0900

    delete commet

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/zen/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/zen/keymap.c
@@ -31,7 +31,6 @@ enum Layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
-  // keyboard layer
   [_QWERTY] = LAYOUT_universal(
     KC_Q           , KC_W          , KC_E          , KC_R          , KC_T              ,                            KC_Y     , KC_U       , KC_I          , KC_O          , KC_P              ,
     LCTL_T(KC_A)   , KC_S          , KC_D          , KC_F          , KC_G              ,                            KC_H     , KC_J       , KC_K          , KC_L          , LCTL_T(KC_ENTER)  ,
@@ -39,7 +38,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LSFT_T(KC_ESC) , KC_LWIN       , LT(2,KC_TAB)  ,LALT_T(KC_LNG2), LT(2,KC_BACKSPACE),LT(3,KC_SPACE),             KC_SPACE , MO(4)      , _______       , _______       , _______           , RSFT_T(KC_LNG1)
   ),
 
-  // mouse layer
   [_MOUSE] = LAYOUT_universal(
     XXXXXXX        , XXXXXXX       , XXXXXXX       , XXXXXXX       , XXXXXXX           ,                            XXXXXXX  , KC_BTN4    , KC_BTN3       , KC_BTN5       , XXXXXXX       ,
     _______        , XXXXXXX       , XXXXXXX       , XXXXXXX       , XXXXXXX           ,                            XXXXXXX  , KC_BTN1    , SCRL_MO       , KC_BTN2       , _______       ,
@@ -47,7 +45,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(_QWERTY)    , XXXXXXX       , XXXXXXX       , _______       , _______           , _______      ,             XXXXXXX  , XXXXXXX    , XXXXXXX       , XXXXXXX       , XXXXXXX       , _______
   ),
 
-  // numbers layer
   [_NUMBERS] = LAYOUT_universal(
     XXXXXXX        , KC_1          , KC_2          , KC_3          , XXXXXXX           ,                            XXXXXXX    , XXXXXXX    , XXXXXXX       , XXXXXXX       , XXXXXXX       ,
     KC_0           , KC_4          , KC_5          , KC_6          , XXXXXXX           ,                            XXXXXXX    , XXXXXXX    , XXXXXXX       , XXXXXXX       , XXXXXXX       ,
@@ -55,7 +52,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______        , XXXXXXX       , XXXXXXX       , XXXXXXX       , XXXXXXX           , XXXXXXX      ,             LCTL(KC_C) , LCTL(KC_V) , XXXXXXX       , XXXXXXX       , XXXXXXX       , _______
   ),
 
-  // symbols layer
   [_SYMBOLS] = LAYOUT_universal(
     LSFT(KC_4)       , LSFT(KC_5)    , LSFT(KC_6)    , LSFT(KC_7)    , LSFT(KC_8)         ,                          LSFT(KC_MINUS)     , LSFT(KC_9)            , LSFT(KC_0)             , KC_EQUAL , LSFT(KC_EQUAL) ,
     LSFT(KC_1)       , LSFT(KC_2)    , LSFT(KC_3)    , KC_GRAVE      , LSFT(KC_GRAVE)     ,                          KC_MINUS           , KC_LEFT_BRACKET       , KC_RIGHT_BRACKET       , KC_QUOTE , LSFT(KC_QUOTE) ,
@@ -63,7 +59,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______          , XXXXXXX       , XXXXXXX       , XXXXXXX       , XXXXXXX            , XXXXXXX   ,              LCTL(KC_X)         , XXXXXXX               , XXXXXXX                , XXXXXXX  , XXXXXXX        , _______
   ),
 
-  // arrows and keyball layer
   [_ARROWS_AND_KEYBALL] = LAYOUT_universal(
     KC_F12           , KC_F1         , KC_F2         ,KC_F3          , XXXXXXX           ,                             XXXXXXX , XXXXXXX    , KC_UP         , XXXXXXX       , XXXXXXX        ,
     KC_F10           , KC_F4         , KC_F5         ,KC_F6          , XXXXXXX           ,                       LCTL(KC_LEFT) , KC_LEFT    , KC_DOWN       , KC_RIGHT      , LCTL(KC_RIGHT) ,
