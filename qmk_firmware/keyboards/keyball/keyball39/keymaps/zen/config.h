c874707afe233841746cc7b4a5147bd8ec81b25c
shoya.noguchi <nogukids@gmail.com>
2024-09-12 17:47:14 +0900

    add comment

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/zen/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/zen/config.h
@@ -35,6 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
+// for auto mouse layer
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
+// for layer
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
