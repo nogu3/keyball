6808acc9150729fdb8fd720d2afec75bf48a0171
shoya.noguchi <nogukids@gmail.com>
2024-10-03 05:05:34 +0900

    up default cpi 500 to 1000

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/zen/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/zen/config.h
@@ -35,6 +35,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
+#define KEYBALL_CPI_DEFAULT 1000
+
 // for auto mouse layer
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
